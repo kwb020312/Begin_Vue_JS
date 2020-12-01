2236fea220089ead67828db3f20e2067c27fecf0
kimwoobin <kwb020312@naver.com>
2020-12-01 13:05:54 +0900

    Create Vue Folder

---
+++ number_base/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<NumberBase />
+	</div>
+</template>
+
+<script>
+import NumberBase from './components/NumberBase.vue';
+
+export default {
+	name: 'App',
+	components: {
+		NumberBase
+	}
+};
+</script>
---
+++ number_base/src/components/NumberBase.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>숫자야구</h1>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+	name: 'NumberBase'
+});
+</script>
