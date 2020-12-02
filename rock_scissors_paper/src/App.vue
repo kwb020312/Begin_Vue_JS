78306d384cd71c2edbc301c767dfc393a469d767
kimwoobin <kwb020312@naver.com>
2020-12-02 10:54:43 +0900

    add IMG

--- rock_scissors_paper/src/App.vue
+++ rock_scissors_paper/src/App.vue
@@ -1,12 +1,30 @@
 <template>
-	<div>
-		RockScissorsPaper
-	</div>
+	<center>
+		<div
+			:style="{
+				background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) 0 0`,
+				width: '142px',
+				height: '200px',
+				backgroundPosition: '0 0'
+			}"
+		></div>
+		<input type="button" value="바위" />
+		<input type="button" value="가위" />
+		<input type="button" value="보" />
+	</center>
 </template>
 
 <script>
+// const Dictionary = {
+// 	바위: '0',
+// 	가위: '-142px',
+// 	보: '-284px'
+// };
 export default {
-	name: 'App'
+	name: 'App',
+	data() {
+		return {};
+	}
 };
 </script>
 
